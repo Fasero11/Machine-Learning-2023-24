58778ad2dadc9d0a38a4d413003724ab523bd16f
Julia <jlopeza2020@alumnos.urjc.es>
2023-10-15 18:28:16 +0200

    added pdf files and Matlab code

--- Matlab/P1/P1_E2.m
+++ Matlab/P1/P1_E2.m
@@ -14,6 +14,7 @@ x3 = [0.69,0.82,0.83,0.29,0.31,0.52,0.33,0.83, ...
     0.81,0.56,1.21,1.22,1.33,1.10,1.75,1.75,1.54,1.34,1.83,1.55];
 
 X = [x1;x2;x3].';
+% + = 1; o = 0
 Y = [1,1,0,1,0,1,1,1,1,1,0,0,0,0,1,0,1,0,0,0];
 
 % --- 2.1 ---
@@ -23,3 +24,61 @@ mdl = fitglm(X,Y,'Distribution','binomial')
 %% 
 
 % --- 2.2 ---
+% representa las entradas
+% Crear una figura para visualizar los datos y las predicciones
+
+% Visualización de datos y predicciones
+figure;
+predictions = predict(mdl, X);
+
+for id = 1:length(predictions)
+    if Y(1,id) == 0 && predictions(id,1) < 0.5
+        % círculo acierto
+        scatter3(X(id, 1), X(id, 2), X(id, 3), 'o', 'filled', 'MarkerFaceColor', 'b');
+    elseif Y(1,id) == 0 && predictions(id,1) > 0.5
+        % círculo fallo
+        scatter3(X(id, 1), X(id, 2), X(id, 3), 'o', 'filled', 'MarkerFaceColor', 'r');
+    elseif Y(1,id) == 1 && predictions(id,1) > 0.5
+        % cruz acierto
+        scatter3(X(id, 1), X(id, 2), X(id, 3), '+', 'MarkerEdgeColor', 'b');
+    elseif Y(1,id) == 1 && predictions(id,1) < 0.5
+        % cruz fallo
+        scatter3(X(id, 1), X(id, 2), X(id, 3), '+', 'MarkerEdgeColor', 'r');
+    end
+    hold on;
+end
+
+
+% Ajustes de la figura
+title('Clasificador de Regresión Logística');
+xlabel('Característica 1');
+ylabel('Característica 2');
+zlabel('Característica 3');
+
+grid on;
+
+
+%% 
+
+% Calcular el error en porcentaje sobre los datos de entrenamiento
+train_predictions = predict(mdl, X);
+
+
+% Convertir las predicciones en etiquetas binarias (0 o 1)
+predicted_classes = round(train_predictions);
+
+% Calcular el número de predicciones que coinciden
+eq = 0;
+for i = 1:length(predicted_classes)
+   if Y(i) == predicted_classes(i)
+       eq = eq + 1;
+   end
+end
+
+
+%Calcular el error obtenido
+err = ((length(predicted_classes) - eq) / length(predicted_classes)) *100;
+ 
+disp("El porcentaje de error de los datos de entrenamiento es: " +err+ "%")
+
+
