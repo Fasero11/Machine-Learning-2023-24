411abe5db02d49ec095d38dfce498c101c049fff
jlopeza2020 <j.lopeza.2020@alumnos.urjc.es>
2023-10-26 20:43:42 +0200

    initialized p2_e1

--- P2/Python_P2/P2_E1.py
+++ P2/Python_P2/P2_E1.py
@@ -7,3 +7,15 @@
 
 if __name__ == '__main__':
 
+    #n = nublado, l = lluvioso, s = soleado
+    prevision = ["n", "l", "n", "l", "s", "l", "s", "s", "n", "s"]
+    # m = media, a = alta, b = baja
+    temperatura = ["m", "a", "b", "m", "a", "b", "b". "a", "b", "m"]
+    marea = ["b", "m", "m", "a", "a", "b", "a", "a", "m", "b"]
+    # me = medio, d = débil, f = fuerte 
+    viento = ["me", "d", "f", "f", "d", "me", "f", "me", "f", "d"]
+    pescar = ["si",]
+    # hacer un data frame
+
+
+#tree.plot_tree8mdl, feature_names = [ ]
