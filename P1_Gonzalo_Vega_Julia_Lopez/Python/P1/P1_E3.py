9ec24613d3f9ba5c46ee4ee518887185d7ab116f
Julia <jlopeza2020@alumnos.urjc.es>
2023-10-27 00:16:15 +0200

    initialzied p2_e1 python

---
+++ P1_Gonzalo_Vega_Julia_Lopez/Python/P1/P1_E3.py
@@ -0,0 +1,79 @@
+# Aprendizaje Automático 2023 - 2024
+
+# Práctica 1 - Ejercicio 3
+
+# Julia López Augusto
+# Gonalo Vega Pérez
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+
+if __name__ == '__main__':
+
+    # Datos Iniciales
+    x1 = [0.89, 0.41, 0.04, 0.75, 0.15, 0.14, 0.61, 0.25, 0.32, 0.40, 1.26,
+           1.68, 1.23, 1.46, 1.38, 1.54, 1.99, 1.76, 1.98, 1.23]
+    x2 = [0.41, 0.39, 0.61, 0.17, 0.19, 0.09, 0.32, 0.77, 0.23, 0.74, 1.53,
+           1.05, 1.76, 1.60, 1.86, 1.99, 1.93, 1.41, 1.00, 1.54]
+    x3 = [0.69, 0.82, 0.83, 0.29, 0.31, 0.52, 0.33, 0.83, 0.81, 0.56, 1.21,
+          1.22, 1.33, 1.10, 1.75, 1.75, 1.54, 1.34, 1.83, 1.55]
+    X = np.column_stack((x1, x2, x3))
+    # '+' = 1
+    #  O = 0
+    Y = np.array([1,1,0,1,0,1,1,1,1,1,0,0,0,0,1,0,1,0,0,0])
+
+
+    # Crear Modelo y entrenarlo
+    mdl = LogisticRegression(penalty=None).fit(X,Y)
+
+    # Resultados (Pesos)
+    coef = mdl.coef_
+    intercept = mdl.intercept_
+
+    print("Coef:" + str(coef))
+    print("Intercept:" + str(intercept))
+
+    # Utilizar modelo entrenado para predecir resultados
+    Ye = mdl.predict(X)
+
+
+    # Calcular el error en porcentaje
+    error_percentage = 100 * (1 - accuracy_score(Y, Ye))
+    print("Error en porcentaje sobre los datos de entrenamiento:", error_percentage, "%")
+
+
+    # Representar graficamente
+    fig = plt.figure
+    ax = plt.axes(projection='3d')
+
+    data_num = 0
+    for data in X:
+        # Si el dato es una cruz
+        if Y[data_num] == 1:
+            #print("plot cross " + str(data_num))
+            # Si la predicción es cruz (acierto): pintar la cruz azul
+            if Ye[data_num] == 1:
+                ax.scatter3D(X[data_num][0], X[data_num][1], X[data_num][2], marker="X", color="blue")
+            # Si la predicción es círculo (error): pintar la cruz roja
+            else:
+                ax.scatter3D(X[data_num][0], X[data_num][1], X[data_num][2], marker="X", color="red") 
+        # Si el dato es un círculo
+        else:
+            #print("plot circle " + str(data_num))
+            # Si la predicción es círculo (acierto): pintar el círculo azul
+            if Ye[data_num] == 0:
+                ax.scatter3D(X[data_num][0], X[data_num][1], X[data_num][2], color="blue")
+            # Si la predicción es cruz (error): pintar el círculo rojo
+            else:
+                ax.scatter3D(X[data_num][0], X[data_num][1], X[data_num][2], color="red")
+
+        data_num += 1
+
+    # Nombrar Ejes
+    ax.set_xlabel('x1', labelpad=20)
+    ax.set_ylabel('x2', labelpad=20)
+    ax.set_zlabel('x3', labelpad=20)
+
+    plt.show()
--- P2/Python_P2/P2_E1.py
+++ P2/Python_P2/P2_E1.py
@@ -3,19 +3,87 @@
 # Práctica 2 - Ejercicio 1
 
 # Julia López Augusto
-# Gonalo Vega Pérez
+# Gonzalo Vega Pérez
+
+# Importa las bibliotecas necesarias
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
 
-    #n = nublado, l = lluvioso, s = soleado
-    prevision = ["n", "l", "n", "l", "s", "l", "s", "s", "n", "s"]
-    # m = media, a = alta, b = baja
-    temperatura = ["m", "a", "b", "m", "a", "b", "b". "a", "b", "m"]
-    marea = ["b", "m", "m", "a", "a", "b", "a", "a", "m", "b"]
-    # me = medio, d = débil, f = fuerte 
-    viento = ["me", "d", "f", "f", "d", "me", "f", "me", "f", "d"]
-    pescar = ["si",]
-    # hacer un data frame
+  # 1.1. Creación del modelo de árbol de decisión
+  data = {
+    'PREVISIÓN': ['NUBLADO', 'LLUVIOSO', 'NUBLADO', 'LLUVIOSO', 'SOLEADO', 'LLUVIOSO', 'SOLEADO', 'SOLEADO', 'NUBLADO', 'SOLEADO'],
+    'TEMPERATURA': ['MEDIA', 'ALTA', 'BAJA', 'MEDIA', 'ALTA', 'BAJA', 'BAJA', 'ALTA', 'BAJA', 'MEDIA'],
+    'MAREA': ['BAJA', 'MEDIA', 'MEDIA', 'ALTA', 'ALTA', 'BAJA', 'ALTA', 'ALTA', 'MEDIA', 'BAJA'],
+    'VIENTO': ['MEDIO', 'DEBIL', 'FUERTE', 'FUERTE', 'DEBIL', 'MEDIO', 'FUERTE', 'MEDIO', 'FUERTE', 'DEBIL'],
+    'PESCAR': ['SI', 'SI', 'NO', 'SI', 'NO', 'SI', 'NO', 'NO', 'NO', 'SI']
+  }
 
+  df = pd.DataFrame(data)
 
-#tree.plot_tree8mdl, feature_names = [ ]
+  # Transformar variables categóricas a numéricas
+  df = pd.get_dummies(df, columns=['PREVISIÓN', 'TEMPERATURA', 'MAREA', 'VIENTO'], drop_first=True)
+
+  # Definir las características (X) y la variable objetivo (y)
+  X = df.drop('PESCAR', axis=1)
+  y = df['PESCAR']
+
+  # Crear el modelo de árbol de decisión
+  model = DecisionTreeClassifier()
+
+  # Ajustar el modelo a los datos
+  model.fit(X, y)
+
+  # a) Para ver qué característica se ha seleccionado como nodo raíz y su importancia, puedes utilizar:
+  print("Características y sus importancias:")
+  feature_importances = dict(zip(X.columns, model.feature_importances_))
+  print(feature_importances)
+
+  # b) Para verificar si existe algún valor de alguna característica que permita pescar independientemente de los demás valores, debes examinar el árbol. Esto dependerá del árbol específico generado.
+
+  # c) Si existe algún atributo que no influye en la decisión, también dependerá del árbol generado y la estructura de tus datos.
+
+  # d) Para adjuntar de forma gráfica el árbol de decisión:
+  plt.figure(figsize=(15, 10))
+  plot_tree(model, feature_names=X.columns, class_names=['No', 'Sí'], filled=True, rounded=True)
+  plt.show()
+
+  # 1.2. Evaluación del modelo con nuevos datos
+  nuevos_datos = {
+    'PREVISIÓN': ['SOLEADO', 'NUBLADO', 'NUBLADO', 'SOLEADO', 'LLUVIOSO', 'LLUVIOSO', 'NUBLADO', 'SOLEADO', 'NUBLADO', 'LLUVIOSO'],
+    'TEMPERATURA': ['ALTA', 'BAJA', 'MEDIA', 'ALTA', 'MEDIA', 'MEDIA', 'BAJA', 'ALTA', 'MEDIA', 'BAJA'],
+    'MAREA': ['MEDIA', 'MEDIA', 'BAJA', 'ALTA', 'BAJA', 'ALTA', 'MEDIA', 'ALTA', 'BAJA', 'MEDIA'],
+    'VIENTO': ['DEBIL', 'MEDIO', 'MEDIO', 'FUERTE', 'MEDIO', 'DEBIL', 'MEDIO', 'DEBIL', 'MEDIO', 'FUERTE'],
+    'PESCAR': ['NO', 'NO', 'SI', 'NO', 'SI', 'SI', 'SI', 'NO', 'SI', 'SI']
+  }
+
+  df_nuevos = pd.DataFrame(nuevos_datos)
+
+  # Transformar variables categóricas a numéricas
+  df_nuevos = pd.get_dummies(df_nuevos, columns=['PREVISIÓN', 'TEMPERATURA', 'MAREA', 'VIENTO'], drop_first=True)
+
+  X_nuevos = df_nuevos.drop('PESCAR', axis=1)
+  y_nuevos = df_nuevos['PESCAR']
+
+  # Predecir con el modelo existente
+  predicciones = model.predict(X_nuevos)
+
+  # Evalúa el modelo con los nuevos datos
+  #accuracy = model.score(X_nuevos, y_nuevos)
+  #print("Porcentaje de acierto en los nuevos datos:", accuracy * 100, "%")
+
+  # Calcular el porcentaje de acierto
+  exactitud = accuracy_score(y_nuevos, predicciones)
+  print("Porcentaje de acierto:", exactitud * 100, "%")
+
+  # 1.3. Modelo de árbol de decisión utilizando los datos de regresión logística
+  # No se proporcionaron datos de regresión logística para este ejercicio, por lo que no se puede generar el modelo.
+
+  # 1.4. Cálculo del error en porcentaje sobre los datos de entrenamiento
+  error = 1 - model.score(X, y)
+  print("Error en porcentaje:", error * 100, "%")
