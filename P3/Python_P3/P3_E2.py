80337412e62eb206bc49c761d06059eaba1a38f9
Fasero11 <gon2d3@gmail.com>
2023-11-02 14:35:44 +0100

    Working P2

---
+++ P3/Python_P3/P3_E2.py
@@ -0,0 +1,57 @@
+# Aprendizaje Automático 2023 - 2024
+
+# Práctica 3 - Ejercicio 2
+
+# Julia López Augusto
+# Gonalo Vega Pérez
+
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import os
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+if __name__ == '__main__':
+
+    current_dir = os.getcwd()
+    train_data = pd.read_csv(str(current_dir) + "/P3/Data_P3/trainingsetPCA.csv")  
+
+    x1 = train_data['X1']
+    x2 = train_data['X2']
+    x3 = train_data['X3']
+    x4 = train_data['X4']
+    x5 = train_data['X5']
+    x6 = train_data['X6']
+    x7 = train_data['X7']
+    x8 = train_data['X8']
+    x9 = train_data['X9']
+    x10 = train_data['X10']
+
+    X = np.column_stack((x1, x2, x3, x4, x5, x6, x7, x8, x9, x10))
+
+    # Normalizamos el set de datos para mejorar el funcionamiento de PCA
+    scaler = StandardScaler()
+    X_normalized = scaler.fit_transform(X)
+
+    
+    keep_components = X.shape[1]
+    info_threshold = 0.9
+
+    retained_info = 1
+    # Vamos quitando características hasta que el porcentaje de información retenida sea menor al 90%
+    while retained_info > info_threshold:
+      pca = PCA(n_components=keep_components)
+      X_reduced = pca.fit_transform(X)
+
+      # Calculamos el porcentaje de información que aporta cada componente
+      variance_explained = pca.explained_variance_ratio_
+
+      # Calculamos el porcentaje de información retenida en total
+      cumulative_variance_explained = variance_explained.cumsum()
+      retained_info = cumulative_variance_explained[-1]
+
+      print("Componentes eliminados: " + str(X.shape[1] - keep_components) + " | Información retenida: "+ str(retained_info))
+
+      keep_components -= 1
